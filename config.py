e0438bae8d105738f84ce8f7ce54fc2bb3827408
youssefaboelwafa <youssef.hossam49@gmail.com>
2026-08-12 20:45:08 +0300

    Add configs

---
+++ config.py
@@ -0,0 +1,13 @@
+import torch
+
+DATASET_PATH = "/scratch/y.aboelwafa/Face-Matching/dataset"
+
+TRAIN_DATASET_PATH = "/scratch/y.aboelwafa/Face-Matching/dataset/train"
+
+VAL_DATASET_PATH = "/scratch/y.aboelwafa/Face-Matching/dataset/val"
+
+CHECKPOINTS_PATH = "/scratch/y.aboelwafa/Face-Matching/checkpoints"
+
+TRAIN_EMBEDDINGS_PATH = "/scratch/y.aboelwafa/Face-Matching/train_embeddings.pkl"
+
+VAL_EMBEDDINGS_PATH = "/scratch/y.aboelwafa/Face-Matching/val_embeddings.pkl"
